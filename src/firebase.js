4eb02ce2cb304bd47a0d269c1d9490fb99414ae1
valdemir-faustino <valfaustino2@hotmail.com>
2023-05-30 16:14:10 -0300

    Firebase Hosting

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,13 @@ import 'firebase/compat/firestore';
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCZ9zjsssPj_RKHWs1gvWLzT8oc5TcpCBE",
-    authDomain: "react-3f57f.firebaseapp.com",
-    projectId: "react-3f57f",
-    storageBucket: "react-3f57f.appspot.com",
-    messagingSenderId: "524070225195",
-    appId: "1:524070225195:web:23db581c8a267bb1fdf16e",    
-    measurementId: "G-9SJQXHMYPL"    
+    apiKey: "AIzaSyDOHtapUkThaJMdFKPMgC-BiHW1Dwl2RmQ",
+  authDomain: "projeto-nuvens.firebaseapp.com",
+  projectId: "projeto-nuvens",
+  storageBucket: "projeto-nuvens.appspot.com",
+  messagingSenderId: "1055650552133",
+  appId: "1:1055650552133:web:be265e439e034f1cad062c",
+  measurementId: "G-1EBFBT7S1H"  
 };
 
 firebase.initializeApp(firebaseConfig);
